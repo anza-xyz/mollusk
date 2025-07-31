a6400125c8e1bd85b4715ac5c561ac624bdb9ba0
L0STE <125566964+L0STE@users.noreply.github.com>
2025-07-31 17:34:05 +0200

    cargo fmt and clippy

--- programs/token/src/state.rs
+++ programs/token/src/state.rs
@@ -2,7 +2,10 @@ use mollusk_svm::Mollusk;
 use solana_account::Account;
 use solana_pubkey::Pubkey;
 use spl_associated_token_account::get_associated_token_address_with_program_id;
-use spl_token::{solana_program::program_pack::Pack, state::{Account as TokenAccount, Mint, AccountState}};
+use spl_token::{
+    solana_program::program_pack::Pack,
+    state::{Account as TokenAccount, AccountState, Mint},
+};
 
 use crate::token;
 
@@ -10,7 +13,13 @@ use crate::token;
 
 // Create a Keyed Account for a Mint with default data
 #[allow(dead_code)]
-pub fn keyed_account_for_mint_default(mollusk: &Mollusk, authority: &Pubkey, decimals: u8, pubkey: Option<Pubkey>, token_program: Option<Pubkey>) -> (Pubkey, Account) {
+pub fn keyed_account_for_mint_default(
+    mollusk: &Mollusk,
+    authority: &Pubkey,
+    decimals: u8,
+    pubkey: Option<Pubkey>,
+    token_program: Option<Pubkey>,
+) -> (Pubkey, Account) {
     let mint_data = Mint {
         mint_authority: Some(*authority).into(),
         supply: 0,
@@ -21,7 +30,7 @@ pub fn keyed_account_for_mint_default(mollusk: &Mollusk, authority: &Pubkey, dec
 
     let mut data = vec![0u8; Mint::LEN];
     Mint::pack(mint_data, &mut data).unwrap();
-    
+
     let account = Account {
         lamports: mollusk.sysvars.rent.minimum_balance(Mint::LEN),
         data,
@@ -35,7 +44,15 @@ pub fn keyed_account_for_mint_default(mollusk: &Mollusk, authority: &Pubkey, dec
 
 // Create a Keyed Account for a Mint
 #[allow(dead_code)]
-pub fn keyed_account_for_mint(mollusk: &Mollusk, authority: &Pubkey, supply: u64, decimals: u8, freeze_authority: Option<Pubkey>, pubkey: Option<Pubkey>, token_program: Option<Pubkey>) -> (Pubkey, Account) {
+pub fn keyed_account_for_mint(
+    mollusk: &Mollusk,
+    authority: &Pubkey,
+    supply: u64,
+    decimals: u8,
+    freeze_authority: Option<Pubkey>,
+    pubkey: Option<Pubkey>,
+    token_program: Option<Pubkey>,
+) -> (Pubkey, Account) {
     let mint_data = Mint {
         mint_authority: Some(*authority).into(),
         supply,
@@ -46,7 +63,7 @@ pub fn keyed_account_for_mint(mollusk: &Mollusk, authority: &Pubkey, supply: u64
 
     let mut data = vec![0u8; Mint::LEN];
     Mint::pack(mint_data, &mut data).unwrap();
-    
+
     let account = Account {
         lamports: mollusk.sysvars.rent.minimum_balance(Mint::LEN),
         data,
@@ -60,7 +77,14 @@ pub fn keyed_account_for_mint(mollusk: &Mollusk, authority: &Pubkey, supply: u64
 
 // Create a Keyed Account for a Token Account with default data
 #[allow(dead_code)]
-pub fn keyed_account_for_token_account_default(mollusk: &Mollusk, mint: &Pubkey, owner: &Pubkey, amount: u64, pubkey: Option<Pubkey>, token_program: Option<Pubkey>) -> (Pubkey, Account) {
+pub fn keyed_account_for_token_account_default(
+    mollusk: &Mollusk,
+    mint: &Pubkey,
+    owner: &Pubkey,
+    amount: u64,
+    pubkey: Option<Pubkey>,
+    token_program: Option<Pubkey>,
+) -> (Pubkey, Account) {
     let account_data = TokenAccount {
         mint: *mint,
         owner: *owner,
@@ -70,8 +94,8 @@ pub fn keyed_account_for_token_account_default(mollusk: &Mollusk, mint: &Pubkey,
         is_native: None.into(),
         delegated_amount: 0,
         close_authority: None.into(),
-    };  
-    
+    };
+
     let mut data = vec![0u8; TokenAccount::LEN];
     TokenAccount::pack(account_data, &mut data).unwrap();
 
@@ -88,7 +112,19 @@ pub fn keyed_account_for_token_account_default(mollusk: &Mollusk, mint: &Pubkey,
 
 // Create a Keyed Account for a Token Account
 #[allow(dead_code)]
-pub fn keyed_account_for_token_account(mollusk: &Mollusk, mint: &Pubkey, owner: &Pubkey, amount: u64, delegate: Option<Pubkey>, state: AccountState, is_native: Option<u64>, delegated_amount: u64, close_authority: Option<Pubkey>, pubkey: Option<Pubkey>, token_program: Option<Pubkey>) -> (Pubkey, Account) {
+pub fn keyed_account_for_token_account(
+    mollusk: &Mollusk,
+    mint: &Pubkey,
+    owner: &Pubkey,
+    amount: u64,
+    delegate: Option<Pubkey>,
+    state: AccountState,
+    is_native: Option<u64>,
+    delegated_amount: u64,
+    close_authority: Option<Pubkey>,
+    pubkey: Option<Pubkey>,
+    token_program: Option<Pubkey>,
+) -> (Pubkey, Account) {
     let account_data = TokenAccount {
         mint: *mint,
         owner: *owner,
@@ -116,16 +152,60 @@ pub fn keyed_account_for_token_account(mollusk: &Mollusk, mint: &Pubkey, owner: 
 
 // Create a Keyed Account for an Associated Token Account with default data
 #[allow(dead_code)]
-pub fn keyed_account_for_associated_token_account_default(mollusk: &Mollusk, mint: &Pubkey, owner: &Pubkey, amount: u64, token_program: Option<Pubkey>) -> (Pubkey, Account) {
-    let associciated_token_address = get_associated_token_address_with_program_id(owner, mint, &token_program.unwrap_or(token::ID));
+pub fn keyed_account_for_associated_token_account_default(
+    mollusk: &Mollusk,
+    mint: &Pubkey,
+    owner: &Pubkey,
+    amount: u64,
+    token_program: Option<Pubkey>,
+) -> (Pubkey, Account) {
+    let associciated_token_address = get_associated_token_address_with_program_id(
+        owner,
+        mint,
+        &token_program.unwrap_or(token::ID),
+    );
 
-    keyed_account_for_token_account_default(mollusk, mint, owner, amount, Some(associciated_token_address), Some(token_program.unwrap_or(token::ID)))
+    keyed_account_for_token_account_default(
+        mollusk,
+        mint,
+        owner,
+        amount,
+        Some(associciated_token_address),
+        Some(token_program.unwrap_or(token::ID)),
+    )
 }
 
 // Create a Keyed Account for an Associated Token Account
 #[allow(dead_code)]
-pub fn keyed_account_for_associated_token_account(mollusk: &Mollusk, mint: &Pubkey, owner: &Pubkey, amount: u64, delegate: Option<Pubkey>, state: AccountState, is_native: Option<u64>, delegated_amount: u64, close_authority: Option<Pubkey>, token_program: Option<Pubkey>) -> (Pubkey, Account) {
-    let associciated_token_address = get_associated_token_address_with_program_id(owner, mint, &token_program.unwrap_or(token::ID));
+pub fn keyed_account_for_associated_token_account(
+    mollusk: &Mollusk,
+    mint: &Pubkey,
+    owner: &Pubkey,
+    amount: u64,
+    delegate: Option<Pubkey>,
+    state: AccountState,
+    is_native: Option<u64>,
+    delegated_amount: u64,
+    close_authority: Option<Pubkey>,
+    token_program: Option<Pubkey>,
+) -> (Pubkey, Account) {
+    let associciated_token_address = get_associated_token_address_with_program_id(
+        owner,
+        mint,
+        &token_program.unwrap_or(token::ID),
+    );
 
-    keyed_account_for_token_account(mollusk, mint, owner, amount, delegate, state, is_native, delegated_amount, close_authority, Some(associciated_token_address), Some(token_program.unwrap_or(token::ID)))
+    keyed_account_for_token_account(
+        mollusk,
+        mint,
+        owner,
+        amount,
+        delegate,
+        state,
+        is_native,
+        delegated_amount,
+        close_authority,
+        Some(associciated_token_address),
+        Some(token_program.unwrap_or(token::ID)),
+    )
 }
